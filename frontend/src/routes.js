7b93bc124f5e482fd4a59c8016347eea054c5f9e
sk-phan <sukiphan97@gmail.com>
2023-04-20 21:45:49 +0300

    set up nav bar

---
+++ frontend/src/routes.js
@@ -0,0 +1,18 @@
+const routes = [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About,
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact,
+    },
+  ];
+  
