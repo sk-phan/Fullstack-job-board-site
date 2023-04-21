a123a88fd5fc378b596b18f91bc655a9077cd03c
sk-phan <sukiphan97@gmail.com>
2023-04-21 22:39:15 +0300

    basic styling for job card

--- frontend/src/components/JobItem.vue
+++ frontend/src/components/JobItem.vue
@@ -1,12 +1,23 @@
 <template>
-   <div>
-        <span> {{ job.title }} </span>
-        <span> Company A </span>
-        <span> {{ job.jobType }} </span>
-        <span> Salary range: {{ job.minSalary }}-{{ job.maxSalary  }}  </span>
-        <span> {{ job.city }}, {{ job.country }} </span>
-        <span> {{ job.createdAt }} </span>
-        <span> {{ job.description }} </span>
+   <div class=" card rounded pa-4 mb-4 ">
+        <div class="d-flex flex-row justify-space-between mb-6">
+            <div class="d-flex flex-row">
+                <img class="rounded mr-4" src="https://images.unsplash.com/photo-1516542076529-1ea3854896f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2371&q=80" alt="company logo" width="50px" height="50px"/>
+                <div class="d-flex flex-column">
+                    <span class="title"> {{ job.title }} </span>
+                    <div>
+                        <span> Company {{ job.username }} </span>
+                        <span> {{ job.jobType }} </span>
+                        <span> Salary range: {{ job.minSalary }}-{{ job.maxSalary  }}  </span>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex flex-column">
+                <span> {{ job.city }}, {{ job.country }} </span>
+                <span> {{ job.createdAt }} </span>
+            </div>
+        </div>
+        <p> {{ job.description }} </p>
    </div>
 </template>
 
@@ -25,3 +36,9 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+    .card {
+        border: #eee solid 1px;
+    }
+</style>
