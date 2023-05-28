17d039ebbcc5a4efa1fc7eba98a51cc8d1650f35
sk-phan <sukiphan97@gmail.com>
2023-05-28 13:50:12 +0300

    add refresh token in reponse and create route to refresh token

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
 const User = require('../model/UserModel')
 
+//Initial login
 loginRouter.post('/', async(req, res) => {
     const { username, password } = req.body
 
@@ -24,13 +25,46 @@ loginRouter.post('/', async(req, res) => {
     
     const token = jwt.sign(
         userForToken, 
-        process.env.SECRET,
-        { expiresIn: 60*60 })
+        "accessSecret",
+        { expiresIn: '60m' }
+        )
+
+    const refreshToken = jwt.sign(
+        userForToken, 
+        "refreshSecret", 
+        { expiresIn: '15m'}
+        );
 
     res
     .status(200)
-    .send({ token, username: user.username, name: user.name })
+    .send({ token, username: user.username, name: user.name, refreshToken })
 
 })
+
+//Post refresh token
+loginRouter.post('/refresh', async(req, res) => {
+    if (!req.body.refreshToken) {
+        return res.status(401).json({ error: 'Refresh token not provided'})
+    }
+
+    try {
+        // Verify the refresh token
+        const decodedToken = jwt.verify(req.body.refreshToken, "refreshSecret");
+
+        const userForToken = {
+            username: decodedToken.username,
+            id: decodedToken.id,
+        };
+        
+        // Generate a new access token
+        const accessToken = jwt.sign(userForToken, "accessSecret", { expiresIn: '60m' });
+        
+        res.json({ accessToken: accessToken });
+
+    }
+    catch (error) {
+        return res.status(401).json({ error: 'Invalid refresh token' });
+    }
+    })
 
 module.exports = loginRouter
