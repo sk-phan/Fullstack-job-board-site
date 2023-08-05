275df2432c1ae06bce7eef5d9ce15e71b2f7b50d
sk-phan <sukiphan97@gmail.com>
2023-08-05 20:07:29 +0300

    update port

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const logger = require('./utils/logger')
 const app = require('./utils/app')
 const config = require('./utils/config')
 
-const options = { host: "0.0.0.0" }
+const options = { host: "0.0.0.0:3000" }
 
 app.listen(config.PORT, options, () => {
     logger.info(`Server running on port ${config.PORT}`)
--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 const MONGO_URI = process.env.MONGO_URI
 const SESSION_SECRET = process.env.SESSION_SECRET;
 
