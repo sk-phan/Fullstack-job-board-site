2ad1ae50a988668e4d4c584978c3dd3b3eb5bea4
sk-phan <sukiphan97@gmail.com>
2023-08-05 20:20:11 +0300

    update port

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 const logger = require('./utils/logger')
 const app = require('./utils/app')
 const config = require('./utils/config')
-const port = 3000; // Change this to 3000
 
-// Define your routes and middleware here
+const options = { host: "0.0.0.0" }
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server listening on http://0.0.0.0:${port}`);
-});
+app.listen(config.PORT, options, () => {
+    logger.info(`Server running on port ${config.PORT}`)
+})
