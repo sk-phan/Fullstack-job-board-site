312005661a8d6e29b09bbb3574dfa65050266c87
sk-phan <sukiphan97@gmail.com>
2024-02-01 13:28:10 +0200

    split codes in filter component

---
+++ frontend/src/components/FilterSideBar.vue
@@ -0,0 +1,214 @@
+<template>
+  <div class="filter pa-6 mr-6 rounded">
+    <v-row class="mx-0 align-center justify-space-between mb-6">
+        <span class="mb-0">Filter</span>
+        <v-btn text class="pa-0" color="primary" @click="$emit('clearFilter')">Clear all</v-btn>
+    </v-row>
+
+    <v-row>
+        <v-col>
+            <span class="label">Experience level</span>
+            <div class="mt-4">
+                <v-row>
+                    <v-col cols="10">
+                        <v-checkbox
+                            class="ma-0 pa-0"
+                            label="Entry level"
+                            color="primary"
+                            hide-details
+                            v-model=" experienceLevel.entry "
+                            @change=" $emit('filterLevel', experienceLevel) "
+                        ></v-checkbox>
+                    </v-col>
+                </v-row>
+
+                <v-row >
+                    <v-col  cols="10">
+                        <v-checkbox
+                            class="ma-0 pa-0"
+                            label="Intermidate"
+                            color="primary"
+                            hide-details
+                            v-model=" experienceLevel.intermidate "
+                            @change=" $emit('filterLevel', experienceLevel) "
+                        ></v-checkbox>
+                    </v-col>
+                </v-row>
+
+                <v-row >
+                    <v-col  cols="10">
+                        <v-checkbox
+                            class="ma-0 pa-0"
+                            label="Expert"
+                            color="primary"
+                            hide-details
+                            v-model=" experienceLevel.expert "
+                            @change=" $emit('filterLevel', experienceLevel) "
+                        ></v-checkbox>
+                    </v-col>
+                </v-row>
+            </div>
+        </v-col>
+    </v-row>
+
+    <v-row class="mt-6">
+        <v-col>
+            <span class="label">Job type</span>
+            <div class="mt-4">
+                <v-row>
+                    <v-col cols="10">
+                        <v-checkbox
+                            class="ma-0 pa-0"
+                            label="Full time"
+                            color="primary"
+                            hide-details
+                            v-model=" jobTypes.fullTime "
+                            @change="$emit('filterJobType', jobTypes)"
+                        ></v-checkbox>
+                    </v-col>
+                </v-row>
+
+                <v-row >
+                    <v-col cols="10">
+                        <v-checkbox
+                            class="ma-0 pa-0"
+                            label="Part time"
+                            color="primary"
+                            hide-details
+                            v-model=" jobTypes.partTime "
+                            @change="$emit('filterJobType', jobTypes)"
+                        ></v-checkbox>
+                    </v-col>
+                </v-row>
+            </div>
+        </v-col>
+    </v-row>
+
+    <v-row class="mt-6">
+        <v-col>
+            <span class="label">Salary range</span>
+            <div class="mt-4">
+                <v-row>
+                    <v-col cols="10">
+                        <v-checkbox
+                            class="ma-0 pa-0"
+                            label="Under 1000€"
+                            color="primary"
+                            hide-details
+                            v-model=" salaryRanges['0-1000'] "
+                            @change=" $emit('filterSalary', salaryRanges) "
+                        ></v-checkbox>
+                    </v-col>
+                </v-row>
+
+                <v-row >
+                    <v-col cols="10">
+                        <v-checkbox
+                            class="ma-0 pa-0"
+                            label="1000€ - 3000€"
+                            color="primary"
+                            hide-details
+                            v-model=" salaryRanges['1000-3000'] "
+                            @change=" $emit('filterSalary', salaryRanges)"
+                        ></v-checkbox>
+                    </v-col>
+                </v-row>
+
+                <v-row >
+                    <v-col  cols="10">
+                        <v-checkbox
+                            class="ma-0 pa-0"
+                            label="3000€ - 5000€"
+                            color="primary"
+                            hide-details
+                            v-model=" salaryRanges['3000-5000'] "
+                            @change=" $emit('filterSalary', salaryRanges) "
+                        ></v-checkbox>
+                    </v-col>
+                </v-row>
+
+                <v-row >
+                    <v-col  cols="10">
+                        <v-checkbox
+                            class="ma-0 pa-0"
+                            label="Up to 5000€"
+                            color="primary"
+                            hide-details
+                            v-model=" salaryRanges[5000] "
+                            @change="$emit('filterSalary', salaryRanges)"
+                        ></v-checkbox>
+                    </v-col>
+                </v-row>     
+            </div>   
+        </v-col>
+    </v-row>
+
+    <v-row class="mt-6">
+        <v-col>
+            <span class="label">Job title</span>
+            <div class="mt-4">
+                <v-row v-for="i in jobCategoriesKeys" :key="i">
+                    <v-col cols="10">
+                        <v-checkbox
+                            class="ma-0 pa-0"
+                            :label="i"
+                            color="primary"
+                            hide-details
+                            v-model=" jobCategories[i] "
+                            @change=" $emit('filterCategories', jobCategories) "
+                        ></v-checkbox>
+                    </v-col>
+                </v-row>
+            </div>
+            
+        </v-col>
+    </v-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FilterSideBar",
+    props: {
+        categories: {
+            type: Object,
+        },
+        experiences: {
+            type: Object,
+        },
+        types: {
+            type: Object,
+        },
+        salary: {
+            type: Object,
+        },
+    },
+    data() {
+        return {
+            jobCategories: this.categories,
+            experienceLevel: this.experiences,
+            jobTypes: this.types,
+            salaryRanges: this.salary,
+        }
+    },
+    computed: {
+        jobCategoriesKeys() {
+            return Object.keys(this.jobCategories)
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    .filter {
+        background:var(--white) !important;
+        height: 100vh !important;
+        overflow: scroll;
+    }
+    .light-text {
+        color: var(--secondary-base) !important;
+    }
+    .label {
+        font-weight: 500;
+    }
+</style>
---
+++ frontend/src/utils/jobFilterOptions.js
@@ -0,0 +1,31 @@
+export const jobCategories = {
+    "Accounting": false,
+    "Administration": false,
+    "Customer Service": false,
+    "Design": false,
+    "Engineering": false,
+    "Finance": false,
+    "Healthcare": false,
+    "Human Resources": false,
+    "Information Technology (IT)": false,
+    "Legal": false,
+    "Marketing": false,
+    "Operations": false,
+    "Sales": false
+    }
+export const experienceLevel = {
+    entry: false,
+    intermidate: false,
+    expert: false
+}
+export const jobTypes = {
+    fullTime: false,
+    partTime: false
+}
+export const salaryRanges = {
+    "0-1000": false,
+    "1000-3000": false,
+    "3000-5000": false,
+    "0-5000": false
+}
+
